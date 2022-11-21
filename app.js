e6ba0b7fc1f83907a9780c91b87ea0c39c1c6ef5
jordan houyon <116800565+Jordan-Houyon@users.noreply.github.com>
2022-11-21 13:35:04 +0100

    exo 66 ok

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
-console.log(random());
+
+let entier=document.getElementById("entier");
+
+let flottant=document.getElementById("flottant");
+
+let notNumber=document.getElementById("non-numerique");
+
+let random=document.getElementById("random");
+
+let trunc=document.getElementById("trunc");
+
+let replace=document.getElementById("replace");
+
+let operande=10;
+
+//------1 --------//
+
+entier.innerHTML =operande*parseInt((entier.innerHTML));
+
+//------2--------//
+
+flottant.innerHTML=operande*parseFloat(flottant.innerHTML);
+flottant.innerHTML=parseInt(entier.innerHTML)+ parseFloat(flottant.innerHTML);
+
+//------3--------//
+
+if(isNaN(notNumber)){
+    notNumber.innerHTML="0";
+}
+
+//------4--------//
+
+random.innerHTML=Math.random();
+
+//------5--------/
+
+trunc.innerHTML=Math.trunc(trunc.innerHTML);
+
+//------6--------//
+
+replace.innerText = replace.innerText.replace('world', 'Jordan Houyon')
+
+
